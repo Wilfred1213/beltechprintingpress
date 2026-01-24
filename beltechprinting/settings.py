8d250abf45236c5285f6ea465c51e2b58203e102
Wilfred Mathias <mathiaswilfred7@gmail.com>
2026-01-24 15:55:37 +0100

    database commit

--- beltechprinting/settings.py
+++ beltechprinting/settings.py
@@ -121,13 +121,34 @@ WSGI_APPLICATION = 'beltechprinting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import socket
 
+# Check if we are running on PythonAnywhere
+if 'pythonanywhere' in socket.gethostname():
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'yourusername$beltech',  # Use your actual DB name
+            'USER': 'yourusername',          # Use your actual username
+            'PASSWORD': 'your_mysql_password', 
+            'HOST': 'yourusername.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+        }
+    }
+else:
+    # Use SQLite locally
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
